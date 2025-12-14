4c85d74fe331bce575600872d625db3b9b0bd632
_FN10_ <mathdudex09@gmail.com>
2025-12-14 16:51:56 -0400

    oh i forgot about this

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import jdk.javadoc.internal.tool.resources.javadoc
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -18,15 +16,21 @@ android {
         }
     }
 
+
+
     defaultConfig {
         applicationId = "fn10.bedrockrmobile"
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0 (a1.5)"
+        versionName = "1.0 (a2.0)"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
+        }
     }
 
     buildTypes {
@@ -51,6 +55,12 @@ android {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/com.google.guava/guava
+    implementation(libs.guava)
+    implementation(libs.commons.io)
+    implementation(libs.commons.lang3)
+    implementation(libs.bedrockr.v811c54da54)
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.lifecycle.runtime.ktx)
@@ -63,8 +73,7 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.coordinatorlayout)
     implementation(libs.constraintlayout)
-    implementation(libs.bedrockr.vmastersnapshot)
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/fn10/bedrockrmobile/Launcher.java
+++ app/src/main/java/fn10/bedrockrmobile/Launcher.java
@@ -2,21 +2,32 @@ package fn10.bedrockrmobile;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-import javax.swing.JOptionPane;
+import java.io.File;
+import java.text.MessageFormat;
+import java.util.logging.Logger;
 
+import fn10.bedrockr.addons.source.supporting.item.ReturnItemInfo;
 import fn10.bedrockr.utils.Greetings;
 import fn10.bedrockr.utils.RFileOperations;
+import fn10.bedrockr.utils.SettingsFile;
 
 public class Launcher extends Activity {
 
-    private static final String Tag = "LaunchPage";
-
     private void setupApp() {
+        Logger.getGlobal().info(MessageFormat.format("bedrockR version: {0}, Java version: {1}, JVM: {2}", RFileOperations.VERSION, System.getProperty("java.version"),
+                System.getProperty("java.vm.name")));
         RFileOperations.setBaseDir(getFilesDir());
+        Logger.getGlobal().info(RFileOperations.getBaseDirectory().getAbsolutePath());
+        SettingsFile settings = SettingsFile.load();
+        RFileOperations.setBaseDir(new File(settings.comMojangPath));
     }
 
     @Override
@@ -32,5 +43,14 @@ public class Launcher extends Activity {
 
         greetingText.setText(greeting.Text);
         greetingText.setTextSize(greeting.Size);
+
+        ImageButton newAddonButton = findViewById(R.id.newAddonButton);
+
+        newAddonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
